f4c7e291e8193b08663667f75a20bdcaf32425f0
Daenys90 <denisse.vidis.stelos>
2024-02-16 16:05:30 -0300

    ejercicio 1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,33 +1,33 @@
 // ejercicio 1 - números
 
 
-var numero1 = parseInt(prompt("Ingrese un número entero difente a 0:"));
+var numero1 = parseInt(prompt("Ingrese un número entero diferente a 0:"));
 var numero2 = parseInt(prompt("Ingrese otro número entero diferente a 0 y al anterior:"));
 
 // Verificar si los números son diferentes y mayores a cero
 if (numero1 !== numero2 && numero1 > 0 && numero2 > 0) {
  
   var suma = numero1 + numero2;
-  console.log("Suma:", suma);
+  document.write("Suma: ", suma, "<hr>");
 
   // Calcular la resta
   var resta = numero1 - numero2;
-  console.log("Resta:", resta);
-
+  document.write("Resta: ", resta, "<hr>");
   // Calcular la división
   var division = numero1 / numero2;
-  console.log("División:", division);
+  document.write("División: " + division,"<hr>");
 
   // Calcular la multiplicación
   var multiplicacion = numero1 * numero2;
-  console.log("Multiplicación:", multiplicacion);
-
+  document.write("Multiplicación: " + multiplicacion, "<hr>");
   // Calcular el módulo
   var modulo = numero1 % numero2;
-  console.log("Módulo:", modulo);
+  document.write("Módulo: ", modulo, "<hr>");
 } else {
-  console.log("Los números deben ser diferentes y mayores a cero.");
+  alert("Los números deben ser diferentes y mayores a cero.");
 }
+
+
 
 // ejercicio 2 - temperatura
 
